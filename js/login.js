4fc5796e08666f84d9f1d05a5e8307391684073c
Pushkin <73908956+PushkinSharma@users.noreply.github.com>
2024-03-22 18:29:38 +0530

    Added validations to login page, fixed new clinic ones

--- js/login.js
+++ js/login.js
@@ -14,5 +14,33 @@ document.addEventListener('DOMContentLoaded', function() {
       hidePasswordIcon.style.display = 'block';
       showPasswordIcon.style.display = 'none';
     });
+
+
   });
+
   
+  function validateLoginForm(){
+    const emailInput = document.getElementById("emailInput");
+    const emailError = document.getElementById("emailError");
+    const email = emailInput.value.trim();
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordInput = document.getElementById('password');
+    const passwordError = document.getElementById('passwordError');
+    const inputWithIcon = document.querySelectorAll('.input-with-icon');
+  
+    if (!emailPattern.test(email)) {
+      emailError.style.display = "block";
+      inputWithIcon[0].style.marginBottom = "0";
+    } else {
+      emailError.style.display = "none";
+      inputWithIcon[0].style.marginBottom = "1rem";
+    }
+
+    if(passwordInput.value.trim() === ""){
+      passwordError.style.display = "block";
+      inputWithIcon[1].style.marginBottom = "0";
+    }else{
+      passwordError.style.display = "none";
+    }
+  }
+
--- js/new-clinic-validations.js
+++ js/new-clinic-validations.js
@@ -1,58 +1,69 @@
-const fieldIDs = [
-  "emailInput",
-  "phoneInput",
-  "clinicName",
-  "address",
-  "city",
-  "state",
-  "country",
-  "category",
-  "productPlan",
-  "planStart",
-  "planEnd",
-  "paymentStatus",
-  "paymentFrequency",
-  "lastPayment",
-  "nextPayment",
-];
+
 
 function validateForm(event) {
-    event.preventDefault();
-  
-    const form = event.target;
-    const formFields = form.querySelectorAll('input, select');
-    const errorDivs = form.querySelectorAll('.global-error');
-    let isValid = true;
-  
-    // Reset all error messages
-    const errorMessages = form.querySelectorAll('.error-message');
-        errorMessages.forEach(errorMessage => {
-        errorMessage.style.display = 'none';
-    });
+  event.preventDefault();
 
-    // Reset all error messages
-    errorDivs.forEach(errorDiv => {
-      errorDiv.textContent = '';
-      errorDiv.style.display = 'none';
-    });
-  
-    // Loop through each form field
-    formFields.forEach((field, index) => {
-      const errorDiv = errorDivs[index];
-  
-      
-      if (
-        (field.tagName === 'INPUT' && field.value.trim() === '') ||
-        (field.tagName === 'SELECT' && field.selectedIndex === 0)
-      ) {
-        isValid = false;
-        errorDiv.textContent = 'This field is required.';
-        errorDiv.style.display = 'block';
-      }
-    });
+  const form = event.target;
+  const formFields = form.querySelectorAll("input, select");
+  const errorDivs = form.querySelectorAll(".global-error");
+  let isValid = true;
 
-    if (isValid) {
-      // If all fields are valid, submit the form
-      form.submit();
+  // Reset all error messages
+  const errorMessages = form.querySelectorAll(".error-message");
+  errorMessages.forEach((errorMessage) => {
+    errorMessage.style.display = "none";
+  });
+
+  // Reset all error messages
+  errorDivs.forEach((errorDiv) => {
+    errorDiv.textContent = "";
+    errorDiv.style.display = "none";
+  });
+
+  // Loop through each form field
+  formFields.forEach((field, index) => {
+    const errorDiv = errorDivs[index];
+
+    if (
+      (field.tagName === "INPUT" && field.value.trim() === "") ||
+      (field.tagName === "SELECT" && field.selectedIndex === 0)
+    ) {
+      isValid = false;
+      errorDiv.textContent = "This field is required.";
+      errorDiv.style.display = "block";
     }
+  });
+
+  if (isValid) {
+    // If all fields are valid, submit the form
+    form.submit();
   }
+}
+
+const emailInput = document.getElementById("emailInput");
+const emailError = document.getElementById("emailError");
+
+emailInput.addEventListener("input", function () {
+  const email = emailInput.value.trim();
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailPattern.test(email)) {
+    emailError.style.display = "block";
+  } else {
+    emailError.style.display = "none";
+  }
+});
+
+const phoneInput = document.getElementById("phoneInput");
+const phoneError = document.getElementById("phoneError");
+
+phoneInput.addEventListener("input", function () {
+  const phone = phoneInput.value.trim();
+  const phonePattern = /^\d{10}$/;
+
+  if (!phonePattern.test(phone)) {
+    phoneError.style.display = "block";
+  } else {
+    phoneError.style.display = "none";
+  }
+});
